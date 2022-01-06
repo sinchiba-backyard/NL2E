f1e31bc00e0e15e7c8e5c61cfa4b706b0c7f9eb8
Ryos Suzuki <ryos@sinby.com>
2022-01-06 18:24:15 +0900

    do.sh がちゃんと動くように変更

--- do.sh
+++ do.sh
@@ -2,14 +2,18 @@
 CWD=`pwd`
 DIR_NAME=`basename $CWD | tr A-Z a-z`
 DOCKER_ID=${DIR_NAME}_mysql_1
+NL2E_DOCKER_ID=${DIR_NAME}_nl2e_1
 IP_ADDR=`docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $DOCKER_ID`
 
 cat << JGEIL > do_init.sh
 grep mysql /etc/hosts > /dev/null
-if [ $? -eq 1 ]; then
+if [ \$? -eq 1 ]; then
     echo $IP_ADDR mysql >> /etc/hosts
 fi
-mysql --host $IP_ADDR -u root -pkhcoder < mysql.txt
+if [ -e mysql.txt ]; then
+    mysql --host $IP_ADDR -u root -pkhcoder < mysql.txt
+    rm mysql.txt
+fi
 cd /KHCoder/khcoder
 /usr/bin/perl ./kh_coder.pl
 JGEIL
@@ -21,4 +25,3 @@ docker cp mysql.txt $NL2E_DOCKER_ID:/
 docker exec -it $NL2E_DOCKER_ID /bin/bash /do_init.sh
 
 mv do_init.sh _do_init.sh
-
