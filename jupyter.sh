135d5fa45064474237f6a2875b3df2e3b775faba
Ryos Suzuki <ryos@sinby.com>
2022-01-07 16:18:49 +0900

    jupyter 起動用のスクリプト追加 memo 追加

---
+++ jupyter.sh
@@ -0,0 +1,2 @@
+echo docker run --rm -p 8888:8888 -v `pwd`/KHCoder/works/jupyter:/home/jovyan/work jupyter/minimal-notebook
+docker run --rm -p 8888:8888 -v `pwd`/KHCoder/works/jupyter:/home/jovyan/work jupyter/minimal-notebook
